89c9d8f9220419010fb83f7ef296dbf494a69712
yiguo-17 <66206441+yiguo-17@users.noreply.github.com>
2021-03-10 15:49:17 -0500

    add basic state to app.js.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import AddingControls from "./component/AddingControls/AddingControls";
+import EditControls from "./component/Edit Controls/EditControls";
+import ItemDisplay from "./component/ItemDisplay/ItemDisplay";
+import Search from "./component/Search/Search"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    items:[
+      {
+        name:"apple",
+        price:1,
+        imageLink:"https://www.applesfromny.com/wp-content/uploads/2020/05/Jonagold_NYAS-Apples2.png",
+        description:"One Apple"
+      }
+    ],
+    templeName: "",
+    templePrice: "",
+    tempImageLink: "",
+    tempDescription: ""
+  }
+
+  render(){
+    return(
+      <div className = "app">
+        
+      </div>
+    )
+  }
 }
 
 export default App;
