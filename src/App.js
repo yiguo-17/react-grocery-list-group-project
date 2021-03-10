fc50e14d67c4ede4aba5e355a2b9fbe45bcd1a64
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2021-03-10 12:48:10 -0700

    added a state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react'
 
-export default App;
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = 
+    
+      [
+        {
+          name: 'Apples',
+          price: '$5',
+          imageLink: '',
+          description: 'Bag of apples',
+
+
+        },
+        {
+          name: 'Oranges',
+          price: '$4',
+          imageLink: '',
+          description: 'Bundle of oranges',
+        },
+      ]
+  
+  }
+  
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
