8358704cb653ead4a27d80436fc359e56d41fa8f
joshualondono <joshualondono@outlook.com>
2021-03-11 14:54:05 -0500

    Update ItemDisplay.js

---
+++ src/component/ItemDisplay/ItemDisplay.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class ItemDisplay extends Component {
+
+    render() {
+      return(
+        <p>
+        <img alt="img" src='https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg'/>
+        <p>display</p>
+      </p>
+      )
+    }
+  
+  }
