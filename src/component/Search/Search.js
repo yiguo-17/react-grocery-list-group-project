4ec4cdb0bfd60d220d0ad8ed76230cd321d987d3
yiguo-17 <66206441+yiguo-17@users.noreply.github.com>
2021-03-15 19:00:09 -0400

    update search with context.

--- src/component/Search/Search.js
+++ src/component/Search/Search.js
@@ -1,19 +1,25 @@
-import React, { Component } from 'react'
-import ItemDisplay from "../ItemDisplay/ItemDisplay"
-
-class Search extends Component {
-    matchKey = (item,props)=>{
-        return(item.name.toLowerCase().includes(props.tempSearch.toLowerCase()))
+import React from 'react';
+import {useState} from 'react';
+function Search(){
+    const [searchTerm, setSearchTerm]= useState('');
+    searchTermHandler = (event)=>{
+        setSearchTerm(event.target.value)
     }
-    render() {
-        return (
-            <div>
-                <input type = "text" placeholder = "Search..."/>
-                props.tempSearch === ""?<ItemDisplay/>:props.items.filter(matchKey)!==""?<ItemDisplay/>:<h1>No Match</h1>
-                
-            </div>
-        )
+    match = (data,key)=>{
+        data.name.toLowerCase().includes(key.toLowerCase())
     }
+    return(
+        <div className = "search">
+            <GlobalContext.Consumer>
+                {(list)=>{
+                    return(
+                        <div>
+                            <input type="text" placeholder="Key Word Here..." onChange={searchTermHandler}/>
+                            <button onClick = {match(list,searchTerm)}>Search</button>
+                        </div>
+                    )
+                }}
+            </GlobalContext.Consumer>
+        </div>
+    )
 }
-
-export default Search;
